4d0f80302cf2a8e407c9fa44e23009e0d486b3a6
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2022-05-12 15:02:25 +0300

    Create hiveRepository.dart

---
+++ lib/data/hiveRepository.dart
@@ -0,0 +1,19 @@
+import 'package:kuranpusula/data/hiveApiClient.dart';
+import 'package:kuranpusula/locator.dart';
+import 'package:kuranpusula/model/beads.dart';
+
+class HiveRepository {
+  HiveApiClient _hiveApiClient = getIt<HiveApiClient>();
+
+  Future<bool> saveBeadsToHive(Beads beads) async {
+    return await _hiveApiClient.saveBeadsToHive(beads);
+  }
+
+  Future<List<Beads>> getSavedBeads() async {
+    return await _hiveApiClient.getSavedBeads();
+  }
+
+  Future<Beads?> getLatestCachedFeeds() async {
+    return await _hiveApiClient.getLatestCachedFeeds();
+  }
+}
