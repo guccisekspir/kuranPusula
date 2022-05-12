acf4615e2473e846b6db90693e3558a8d1e08877
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2022-05-12 15:02:44 +0300

    easy loading init

--- lib/landPage.dart
+++ lib/landPage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:kuranpusula/helpers/sizeHelper.dart';
 import 'package:kuranpusula/helpers/themeHelper.dart';
+import 'package:kuranpusula/pages/homePage/homePage.dart';
 
 class LandPage extends StatefulWidget {
   const LandPage({Key? key}) : super(key: key);
@@ -13,6 +15,25 @@ class LandPage extends StatefulWidget {
 class _LandPageState extends State<LandPage> {
   bool isInit = false;
 
+  void configLoading() {
+    EasyLoading.instance
+      ..displayDuration = const Duration(milliseconds: 10)
+      ..animationDuration = const Duration(milliseconds: 10)
+      ..indicatorType = EasyLoadingIndicatorType.cubeGrid
+      ..animationStyle = EasyLoadingAnimationStyle.scale
+      ..loadingStyle = EasyLoadingStyle.custom
+      ..maskType = EasyLoadingMaskType.custom
+      ..indicatorSize = 45.0
+      ..radius = 20.0
+      ..progressColor = ThemeHelper().primaryColor
+      ..backgroundColor = ThemeHelper().backgroundColor
+      ..indicatorColor = ThemeHelper().primaryColor
+      ..textColor = Colors.pinkAccent
+      ..maskColor = Colors.black.withOpacity(0.7)
+      ..userInteractions = true
+      ..dismissOnTap = false;
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -22,6 +43,7 @@ class _LandPageState extends State<LandPage> {
     SchedulerBinding.instance!.addPostFrameCallback((_) {
       ThemeHelper(fetchedContext: context);
       SizeHelper(fetchedContext: context);
+      configLoading();
       setState(() {
         isInit = true;
       });
@@ -30,6 +52,6 @@ class _LandPageState extends State<LandPage> {
 
   @override
   Widget build(BuildContext context) {
-    return isInit ? Container() : const SizedBox();
+    return isInit ? Scaffold(body: HomePage()) : const SizedBox();
   }
 }
